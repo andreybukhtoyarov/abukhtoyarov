b38f2bf378fb9d55cb9684ea96d54f56f619202a
Andrey Bukhtoyarov <andreymedoed@gmail.com>
2017-12-20 16:51:46 +0300

    1.3. JavaDoc. Code convention [#4087]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,7 +1,18 @@
-
 package ru.job4j;
 
+/**
+*Class Класс Calculate для вывода текста самой простой прграммы на экран в консоли.
+*@autor abukhtoyarov
+*@since 20.12.2017
+*@version 1.0
+*/
 public class Calculate {
+	
+	/**
+	*Точка входа в программу. Печатает "Привет Мир".
+	*@param агрумент типа массив строк.
+	*@return ничего не возвращает.
+	*/
 	public static void main(String[] args) {
 		System.out.println("Hello world");
 	}
---
+++ chapter_001/src/main/java/ru/job4j/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for ferst program.
+*
+* @author Andrey Bukhtoyarov (andreymedoed@gmail.com)
+* @version $Id$
+* @since 0.1
+*/
+package ru.job4j;
