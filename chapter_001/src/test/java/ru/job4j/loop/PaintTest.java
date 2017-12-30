d59e42e2d6afd3e040322f2b5fb7417fdd1b42df
Andrey Bukhtoyarov <34687402+andreybukhtoyarov@users.noreply.github.com>
2017-12-30 14:41:29 +0300

    Add files via upload
    111

---
+++ chapter_001/src/test/java/ru/job4j/loop/PaintTest.java
@@ -0,0 +1,61 @@
+package ru.job4j.loop;
+
+import org.junit.Test;;
+import java.util.StringJoiner;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+/**
+ * 
+ *@author Andrey Bukhtoyarov (andreymedoed@gmail.com)..
+ *@version %Id%.
+ *@since 0.1.
+ */
+public class PaintTest {
+	@Test
+	public void whenPyramidHeightFive() {
+		Paint paint = new Paint();
+		String result = paint.pyramid(5);
+		System.out.println(result);
+		assertThat(result,
+				is(new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
+					.add("    ^    ")
+					.add("   ^^^   ")
+					.add("  ^^^^^  ")
+					.add(" ^^^^^^^ ")
+					.add("^^^^^^^^^")
+					.toString()
+				)
+		
+		);
+	}
+	
+	@Test
+	public void whenPyramidHeightFour() {
+		Paint paint = new Paint();
+		String result = paint.pyramid(4);
+		System.out.println(result);
+		assertThat(result,
+				is(new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
+					.add("   ^   ")
+					.add("  ^^^  ")
+					.add(" ^^^^^ ")
+					.add("^^^^^^^")
+					.toString()
+				)
+		
+		);
+	}
+	
+	@Test
+	public void whenRightTrlHeightFour() {
+		Paint paint = new Paint();
+		String result = paint.pyramid(4);
+		System.out.println(result);
+		String ln = System.lineSeparator();
+		assertThat(result,
+				is(String.format("^   %s^^  %s^^^ %s^^^^", ln, ln, ln))
+		);
+	}
+}
