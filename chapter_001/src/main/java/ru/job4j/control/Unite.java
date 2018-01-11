0754eff1ebcb373441d3ed1ca91c76f002f3c365
Andrey Bukhtoyarov <andreymedoed@gmail.com>
2018-01-11 22:33:56 +0300

    Задание с контрольных вопросов.

--- chapter_001/src/main/java/ru/job4j/control/Unite.java
+++ chapter_001/src/main/java/ru/job4j/control/Unite.java
@@ -14,32 +14,23 @@ public class Unite {
      *@return united array.
      */
     public int[] uniteArray(int[] first, int[] second) {
-        int len = first.length + second.length;
-        int[] array = new int[len];
+        int[] array = new int[first.length + second.length];
 
         int indexFirst = 0;
         int indexSecond = 0;
-
-        for (; indexFirst < first.length && indexSecond < second.length;) {
-            if (first[indexFirst] < second[indexSecond]) {
-                array[indexFirst + indexSecond] = first[indexFirst];
-                indexFirst++;
-            } else {
-                array[indexFirst + indexSecond] = second[indexSecond];
-                indexSecond++;
+        while(array.length > indexFirst + indexSecond) {
+            if (indexFirst < first.length && indexSecond < second.length) {
+                if(first[indexFirst] < second[indexSecond]) {
+                    array[indexFirst + indexSecond] = first[indexFirst++];
+                } else {
+                    array[indexFirst + indexSecond] = second[indexSecond++];
+                }
+            } else if (indexFirst < first.length) {
+                array[indexFirst + indexSecond] = first[indexFirst++];
+            } else if (indexSecond < second.length) {
+                array[indexFirst + indexSecond] = second[indexSecond++];
             }
         }
-
-        for (; indexFirst < first.length;) {
-            array[indexFirst + indexSecond] = first[indexFirst];
-            indexFirst++;
-        }
-
-        for (; indexSecond < second.length;) {
-            array[indexFirst + indexSecond] = second[indexSecond];
-            indexSecond++;
-        }
-
         return array;
     }
 }
