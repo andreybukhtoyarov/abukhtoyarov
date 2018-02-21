8cb1cac03109ccd8c65c80b4c76ff9c27978eb52
Andrey Bukhtoyarov <andreymedoed@gmail.com>
2018-02-21 14:59:24 +0300

    Кофемашина.[#39472]

--- chapter_002/src/main/java/ru/job4j/control/CoffeeMachine.java
+++ chapter_002/src/main/java/ru/job4j/control/CoffeeMachine.java
@@ -7,6 +7,14 @@ package ru.job4j.control;
  * @since 0.1.
  */
 public class CoffeeMachine {
+    /**Left column - denomination of a coins, right column - count of coins. */
+    private final int[][] coins = new int[][] {
+            {10, 0},
+            {5, 0},
+            {2, 0},
+            {1, 0}
+    };
+
     /**
      * This method return array of coins.
      * @param value - received money.
@@ -16,27 +24,21 @@ public class CoffeeMachine {
     int[] changes(int value, int price) {
         int[] change = null;
         if (value - price > 0) {
-            final int[][] coin = new int[][] {
-                    {10, 0},
-                    {5, 0},
-                    {2, 0},
-                    {1, 0}
-            };
             int difference = value - price;
-            for (int index = 0; index < coin.length; ++index) {
-                coin[index][1] = difference / coin[index][0];
-                difference = difference - coin[index][0] * coin[index][1];
+            for (int index = 0; index < coins.length; ++index) {
+                coins[index][1] = difference / coins[index][0];
+                difference = difference - coins[index][0] * coins[index][1];
             }
             int changeLength = 0;
-            for (int[] aCoin1 : coin) {
-                changeLength = changeLength + aCoin1[1];
+            for (int[] coin : coins) {
+                changeLength = changeLength + coin[1];
             }
             change = new int[changeLength];
             int indexChange = 0;
-            for (int[] aCoin : coin) {
-                if (aCoin[1] > 0) {
-                    for (int indexTwo = 0; indexTwo < aCoin[1]; ++indexTwo) {
-                        change[indexChange] = aCoin[0];
+            for (int[] coin : coins) {
+                if (coin[1] > 0) {
+                    for (int indexTwo = 0; indexTwo < coin[1]; ++indexTwo) {
+                        change[indexChange] = coin[0];
                         ++indexChange;
                     }
                 }
