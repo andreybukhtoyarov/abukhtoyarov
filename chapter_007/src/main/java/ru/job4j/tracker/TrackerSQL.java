66b4f7f9f34ad3f377db75ba9a7e9a408c82b7fa
Andrey Bukhtoyarov <andreymedoed@gmail.com>
2019-08-07 18:04:01 +0300

    Трекер SQL [#1734]

--- chapter_007/src/main/java/ru/job4j/tracker/TrackerSQL.java
+++ chapter_007/src/main/java/ru/job4j/tracker/TrackerSQL.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import ru.job4j.trackerrefactor.ITracker;
 import ru.job4j.trackerrefactor.Item;
 
@@ -17,6 +19,10 @@ import java.util.Properties;
  */
 public class TrackerSQL implements ITracker, AutoCloseable {
     /**
+     * Логер.
+     */
+    private static final Logger LOG = LogManager.getLogger(TrackerSQL.class.getName());
+    /**
      * Соединение с базод данных.
      */
     private Connection connection;
@@ -29,12 +35,12 @@ public class TrackerSQL implements ITracker, AutoCloseable {
 
     /**
      * Инициализация класса TrackerSQL.
-     * Читает конфиг из app.properties и создает на основе конфига соединение с БД.
+     * Читает конфиг из trackerSQL.properties и создает на основе конфига соединение с БД.
      * Далее вызывается метод private void initDB(), если в БД нет структуры, он ее создаст.
      * @return true если соединение создано.
      */
     public boolean init() {
-        try (final InputStream in = TrackerSQL.class.getClassLoader().getResourceAsStream("app.properties")) {
+        try (final InputStream in = TrackerSQL.class.getClassLoader().getResourceAsStream("trackerSQL.properties")) {
             Properties config = new Properties();
             config.load(in);
             Class.forName(config.getProperty("driver-class-name"));
@@ -46,6 +52,7 @@ public class TrackerSQL implements ITracker, AutoCloseable {
             this.connection.setAutoCommit(false);
             initDB(this.structure);
         } catch (Exception e) {
+            LOG.error("trace message {}", e);
             throw new IllegalStateException(e);
         }
         return this.connection != null;
@@ -53,6 +60,7 @@ public class TrackerSQL implements ITracker, AutoCloseable {
 
     /**
      * Создает Структуру в БД, если в БД нет ни одной таблицы.
+     * @param request - SQL запрос, создающий структуру БД.
      */
     private void initDB(final StringBuilder request) {
         try {
@@ -60,11 +68,11 @@ public class TrackerSQL implements ITracker, AutoCloseable {
             String[] types = {"TABLE"};
             try (final ResultSet tables = metaData.getTables(null, null, "items", types)) {
                 if (!tables.next()) {
-                    reqUpdate(request.toString(), null, null);
+                    reqUpdate(request.toString());
                 }
             }
         } catch (SQLException e) {
-            e.printStackTrace();
+            LOG.error("trace message {}", e);
         }
     }
 
@@ -77,72 +85,184 @@ public class TrackerSQL implements ITracker, AutoCloseable {
 
     /**
      * Создает PreparedStatement для this.connection и выполняет executeUpdate() SQL запрос,
-     * переданный в качестве первого параметра метода.
-     * Если входящий параметр item не null, добавляет в запрос имя, описание и дату создания из item.
-     * Если так же не null входящий параметр id, то добавляет в запрос 4 параметром заданный id.
-     * Если входящий параметр item null, а входящий параметр id не null, то добавляется в запрос 1 параметром id.
+     * переданный в качестве параметра метода.
      * @param req - SQL запрос.
-     * @param item - объект типа Item.
-     * @param id - id в БД.
      */
-    private void reqUpdate(String req, Item item, Integer id) {
+    private void reqUpdate(String req) {
         try (final PreparedStatement statement = this.connection.prepareStatement(req)) {
-            if (item != null) {
-                statement.setString(1, item.getName());
-                statement.setString(2, item.getDescription());
-                statement.setLong(3, item.getCreate());
-            }
-            if (id != null && item != null) {
-                statement.setInt(4, id);
-            } else if (id != null) {
-                statement.setInt(1, id);
-            }
             statement.executeUpdate();
             this.connection.commit();
         } catch (SQLException e) {
             try {
                 this.connection.rollback();
             } catch (SQLException e1) {
-                e1.printStackTrace();
+                LOG.error("trace message {}", e1);
             }
-            e.printStackTrace();
+            LOG.error("trace message {}", e);
         }
+    }
+
+    /**
+     * Создает PreparedStatement для this.connection и выполняет executeUpdate() SQL запрос,
+     * переданный в качестве первого параметра метода.
+     * Добавляет в запрос первым параметром заданный id.
+     * @param req - SQL запрос.
+     * @param id - id в БД.
+     */
+    private void reqUpdate(String req, Integer id) {
+        try (final PreparedStatement statement = this.connection.prepareStatement(req)) {
+            if (id != null) {
+                statement.setInt(1, id);
+                statement.executeUpdate();
+                this.connection.commit();
+            }
+        } catch (SQLException e) {
+            try {
+                this.connection.rollback();
+            } catch (SQLException e1) {
+                LOG.error("trace message {}", e1);
+            }
+            LOG.error("trace message {}", e);
+        }
+    }
+
+    /**
+     * Создает PreparedStatement для this.connection и выполняет executeUpdate() SQL запрос,
+     * переданный в качестве первого параметра метода.
+     * Добавляет в запрос имя, описание и дату создания из item.
+     * А так же добавляет четвертым параметром id.
+     * @param req - SQL запрос.
+     * @param item - объект типа Item.
+     * @param id - id в БД.
+     */
+    private void reqUpdate(String req, Item item, Integer id) {
+        try (final PreparedStatement statement = this.connection.prepareStatement(req)) {
+            if (id != null && item != null) {
+                statement.setString(1, item.getName());
+                statement.setString(2, item.getDescription());
+                statement.setLong(3, item.getCreate());
+                statement.setInt(4, id);
+                statement.executeUpdate();
+                this.connection.commit();
+            }
+        } catch (SQLException e) {
+            try {
+                this.connection.rollback();
+            } catch (SQLException e1) {
+                LOG.error("trace message {}", e1);
+            }
+            LOG.error("trace message {}", e);
+        }
+    }
+
+    /**
+     * Создает PreparedStatement для this.connection и выполняет executeUpdate() SQL запрос,
+     * переданный в качестве первого параметра метода.
+     * Добавляет в запрос имя, описание и дату создания из item.
+     * @param req - SQL запрос.
+     * @param item - объект типа Item.
+     */
+    private void reqUpdate(String req, Item item) {
+        try (final PreparedStatement statement = this.connection.prepareStatement(req)) {
+            if (item != null) {
+                statement.setString(1, item.getName());
+                statement.setString(2, item.getDescription());
+                statement.setLong(3, item.getCreate());
+                statement.executeUpdate();
+                this.connection.commit();
+            }
+        } catch (SQLException e) {
+            try {
+                this.connection.rollback();
+            } catch (SQLException e1) {
+                LOG.error("trace message {}", e1);
+            }
+            LOG.error("trace message {}", e);
+        }
+    }
+
+    /**
+     * Добавляет все заявки из resultSet в переданный, в качестве параметра, list.
+     * @param list - коллекция типа List.
+     * @param resultSet - ResultSet
+     * @throws SQLException ResultSet может выкинуть.
+     */
+    private void addItem(List<Item> list, ResultSet resultSet) throws SQLException {
+        while (resultSet.next()) {
+            Item item = new Item(resultSet.getString("name"),
+                    resultSet.getString("description"),
+                    resultSet.getLong("create_date")
+            );
+            item.setId(String.valueOf(resultSet.getInt("id")));
+            list.add(item);
+        }
+    }
+
+    /**
+     * Создает PreparedStatement для this.connection и выполняет executeQuery() SQL запрос,
+     * переданный в качестве параметра метода. Возвращает List<Item> с результатом запроса.
+     * Если результатов нет - вернет пустую коллекцию.
+     * @param req - SQL запрос.
+     * @return - Возвращает List<Item> с результатом запроса, если результатов нет - вернет пустую коллекцию.
+     */
+    private List<Item> reqSelect(String req) {
+        List<Item> result = new ArrayList<>();
+        try (final PreparedStatement statement = this.connection.prepareStatement(req)) {
+            try (final ResultSet resultSet = statement.executeQuery()) {
+                addItem(result, resultSet);
+            }
+        } catch (SQLException e) {
+            LOG.error("trace message {}", e);
+        }
+        return result;
     }
 
     /**
      * Создает PreparedStatement для this.connection и выполняет executeQuery() SQL запрос,
      * переданный в качестве первого параметра метода. Возвращает List<Item> с результатом запроса.
      * Если результатов нет - вернет пустую коллекцию.
-     * Если второй параметр метода name не null - первым параметром SQL запроса устанавливается name.
-     * Если name is null, а id не null - первым параметром SQL запроса устанавливается id.
+     * Первым параметром SQL запроса устанавливается name.
      * @param req - SQL запрос.
-     * @param name - имя заявки.
-     * @param id - id в БД.
      * @return - Возвращает List<Item> с результатом запроса, если результатов нет - вернет пустую коллекцию.
      */
-    private List<Item> reqSelect(String req, String name, Integer id) {
+    private List<Item> reqSelect(String req, String name) {
         List<Item> result = new ArrayList<>();
         try (final PreparedStatement statement = this.connection.prepareStatement(req)) {
             if (name != null) {
                 statement.setString(1, name);
-            } else if (id != null) {
-                statement.setInt(1, id);
             }
             try (final ResultSet resultSet = statement.executeQuery()) {
-                while (resultSet.next()) {
-                    Item item = new Item(resultSet.getString("name"),
-                            resultSet.getString("description"),
-                            resultSet.getLong("create_date")
-                    );
-                    item.setId(String.valueOf(resultSet.getInt("id")));
-                    result.add(item);
-                }
+                addItem(result, resultSet);
             }
         } catch (SQLException e) {
-            e.printStackTrace();
+            LOG.error("trace message {}", e);
         }
         return result;
     }
+
+    /**
+     * Создает PreparedStatement для this.connection и выполняет executeQuery() SQL запрос,
+     * переданный в качестве первого параметра метода. Возвращает List<Item> с результатом запроса.
+     * Если результатов нет - вернет пустую коллекцию.
+     * Первым параметром SQL запроса устанавливается name.
+     * @param req - SQL запрос.
+     * @return - Возвращает List<Item> с результатом запроса, если результатов нет - вернет пустую коллекцию.
+     */
+    private List<Item> reqSelect(String req, Integer id) {
+        List<Item> result = new ArrayList<>();
+        try (final PreparedStatement statement = this.connection.prepareStatement(req)) {
+            if (req != null && id != null) {
+                statement.setInt(1, id);
+                try (final ResultSet resultSet = statement.executeQuery()) {
+                    addItem(result, resultSet);
+                }
+            }
+        } catch (SQLException e) {
+            LOG.error("trace message {}", e);
+        }
+        return result;
+    }
+
 
     /**
      * Добавить item в БД.
@@ -151,7 +271,7 @@ public class TrackerSQL implements ITracker, AutoCloseable {
      */
     @Override
     public Item add(Item item) {
-        reqUpdate("INSERT INTO items (name, description, create_date) VALUES (?, ?, ?)", item, null);
+        reqUpdate("INSERT INTO items (name, description, create_date) VALUES (?, ?, ?)", item);
         return item;
     }
 
@@ -175,7 +295,7 @@ public class TrackerSQL implements ITracker, AutoCloseable {
      */
     @Override
     public void delete(String id) {
-        reqUpdate("DELETE FROM items WHERE id = ?", null, Integer.valueOf(id));
+        reqUpdate("DELETE FROM items WHERE id = ?", Integer.valueOf(id));
     }
 
     /**
@@ -184,7 +304,7 @@ public class TrackerSQL implements ITracker, AutoCloseable {
      */
     @Override
     public List<Item> getAll() {
-        return reqSelect("SELECT * FROM items", null, null);
+        return reqSelect("SELECT * FROM items");
     }
 
     /**
@@ -194,7 +314,7 @@ public class TrackerSQL implements ITracker, AutoCloseable {
      */
     @Override
     public List<Item> findByName(String key) {
-        return reqSelect("SELECT * FROM items WHERE name = ?;", key, null);
+        return reqSelect("SELECT * FROM items WHERE name = ?;", key);
     }
 
     /**
@@ -205,7 +325,7 @@ public class TrackerSQL implements ITracker, AutoCloseable {
     @Override
     public Item findById(String id) {
         Item item = new Item();
-        List<Item> items = reqSelect("SELECT * FROM items WHERE id = ?;", null, Integer.valueOf(id));
+        List<Item> items = reqSelect("SELECT * FROM items WHERE id = ?;", Integer.valueOf(id));
         if (!items.isEmpty()) {
             item = items.get(0);
         }
